c56ea8391c5580a56ea7fcabbd9dd5712ec626c3
darinvi <d.v.ivanov98@gmail.com>
2022-11-29 17:50:49 +0200

    ready with RVOL calculation

--- relative_volume.py
+++ relative_volume.py
@@ -5,9 +5,15 @@ def client(symbol):
     #returns time series for open,high,low,close,volume,time
     epoch = int(time.time())
     client = finnhub.Client(api_key="caq8suiad3iecj6adq7g")
-    data = client.stock_candles(symbol,"1",epoch-1300000,epoch)
+    data = client.stock_candles(symbol,"1",epoch-2000000,epoch)
     return data
 
+def main(symbol):
+    candles = getVolumeAndTime(symbol)
+    candles = mapUnixToDate(candles)
+    candles = getRegularTradingSession(candles)
+    calculateRelativeVolume(candles)
+    
 def getVolumeAndTime(symbol):
     #only returns volume and time for the purposes of this program
     data = client(symbol)
@@ -38,24 +44,25 @@ def getRegularTradingSession(lst):
         ct[1].hour<lst[-1][1].hour or (ct[1].hour==lst[-1][1].hour and ct[1].minute<=lst[-1][1].minute)]
     return current_time
 
-def separateTodaysData(lst):
+def separateData(lst):
     #separate today's data from data for the past days so I can compare
+    #Only returns closes
+    rolling_days = list(dict.fromkeys([days[1].day for days in lst]))[-11:-1]
+    past_closes = [item[0] for item in lst if item[1].day in rolling_days]
+    today_closes = [item[0] for item in lst if item[1].day==lst[-1][1].day]
+    return past_closes,today_closes
 
-    pass
+def calculateAverageVolume(lst):
+    print(sum(lst))
+    return sum(lst)/len(lst)
 
-def ProperDates(length,lst):
-    days = set([lst[1].day for days in lst])
-    for day in days:
-        print(day)
-    print(len(day))
+def calculateRelativeVolume(lst):
+    past,today = separateData(lst)
+    past_avg = calculateAverageVolume(past)
+    today_avg = calculateAverageVolume(today)
+    rvol = today_avg/past_avg
+    print(f"RVOL = {rvol}")
 
-def getProperLength(lst):
-    pass
-
-candles = getVolumeAndTime("SPY")
-candles = mapUnixToDate(candles)
-candles = getRegularTradingSession(candles)
-
-# for candle in candles:
-#     print(candle)
-# print(len(candles))
+while True:
+    symbol = input("Symbol: ").upper()
+    main(symbol)
