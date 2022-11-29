3dfbb81126fbed84aac91a76724229a42dcd790a
darinvi <d.v.ivanov98@gmail.com>
2022-11-29 16:01:13 +0200

    add functions to filter intraday data

--- data_prep.py
+++ data_prep.py
@@ -1,5 +1,5 @@
 import finnhub
-import time
+import datetime,pytz,time
 
 def client(symbol):
     #returns time series for open,high,low,close,volume,time
@@ -13,7 +13,49 @@ def getVolumeAndTime(symbol):
     data = client(symbol)
     if not len(data['t']) == len(data['v']):
         print("finnhub wrong response")
-    filtered = [element for element in list(zip(list(data['v']),list(data['t'])))]
+        #add recursion to main here
+    filtered = [element for element in list(map(list,zip(list(data['v']),list(data['t']))))]
     return filtered
 
-def getRegularTradingHours(data):
+def unixToNyDate(lst_element):
+    #finnhub returns unix, much easier to filter based on hour and minute.
+    ny_timezone = pytz.timezone('America/New_York')
+    unix_to_datetime = datetime.datetime.utcfromtimestamp(lst_element[1]).replace(tzinfo=pytz.utc)
+    as_date = unix_to_datetime.astimezone(ny_timezone)
+    lst_element[1] = as_date
+    return lst_element
+
+def mapUnixToDate(lst):
+    lst = list(map(unixToNyDate,lst))
+    return lst
+
+def getRegularTradingSession(lst):
+    #this one fillters data for the whole trading session.
+    regular_session = [rs for rs in lst if \
+        (rs[1].hour==9 and rs[1].minute>29) or rs[1].hour in range(10,16)]
+    #this one fillters it up to the current moment.
+    current_time = [ct for ct in regular_session if \
+        ct[1].hour<lst[-1][1].hour or (ct[1].hour==lst[-1][1].hour and ct[1].minute<=lst[-1][1].minute)]
+    return current_time
+
+def separateTodaysData(lst):
+    #separate today's data from data for the past days so I can compare
+
+    pass
+
+def ProperDates(length,lst):
+    days = set([lst[1].day for days in lst])
+    for day in days:
+        print(day)
+    print(len(day))
+
+def getProperLength(lst):
+    pass
+
+candles = getVolumeAndTime("SPY")
+candles = mapUnixToDate(candles)
+candles = getRegularTradingSession(candles)
+
+# for candle in candles:
+#     print(candle)
+# print(len(candles))
